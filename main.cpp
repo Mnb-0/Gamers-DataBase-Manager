fde08cb0fbd4037384454d69614a593648f6cbee
Muneeb <150353438+Mnb-0@users.noreply.github.com>
2024-10-29 20:00:40 +0500

    Created Games_Played Class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std;
+
+class Games_Played
+{
+    public:
+        string gameID;
+        float hoursPlayed;
+        int achievementsUnlocked;
+};
+
 int main()
 {
 
